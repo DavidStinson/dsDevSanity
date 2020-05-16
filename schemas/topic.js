85a56a90c4b675bd25f0bb2dd094206204d477bf
David Stinson <mail@davidstinson.dev>
2020-05-15 19:49:09 -0500

    chore(format): correct formatting in schemas

--- schemas/topic.js
+++ schemas/topic.js
@@ -1,20 +1,20 @@
 export default {
-  name: 'topic',
-  title: 'Topic',
-  type: 'document',
+  name: "topic",
+  title: "Topic",
+  type: "document",
   fields: [
     {
-      name: 'title',
+      name: "title",
       description: "The title for this topic",
-      title: 'Title',
-      type: 'string'
+      title: "Title",
+      type: "string",
       validation: (Rule) => Rule.required(),
     },
     {
-      name: 'image',
+      name: "image",
       description: "Provide an optional image for this topic",
-      title: 'Image',
-      type: 'image',
+      title: "Image",
+      type: "image",
     },
-  ]
-}
+  ],
+};
