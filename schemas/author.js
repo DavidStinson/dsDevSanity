7dae364f11a39e54d9e5a8e289d295300ad829f7
David Stinson <mail@davidstinson.dev>
2020-05-16 11:11:52 -0500

    build(schema): remove author slug and block content bio

--- schemas/author.js
+++ schemas/author.js
@@ -11,17 +11,6 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "slug",
-      description: "The uri of this author",
-      title: "Slug",
-      type: "slug",
-      validation: (Rule) => Rule.required(),
-      options: {
-        source: "name",
-        maxLength: 96,
-      },
-    },
-    {
       name: "image",
       description: "An avatar for this author",
       title: "Image",
@@ -52,18 +41,10 @@ export default {
     },
     {
       name: "bio",
-      description: "A bio for this author",
+      description: "A short bio for this author",
       title: "Bio",
-      type: "array",
+      type: "string",
       validation: (Rule) => Rule.required(),
-      of: [
-        {
-          title: "Block",
-          type: "block",
-          styles: [{ title: "Normal", value: "normal" }],
-          lists: [],
-        },
-      ],
     },
   ],
   preview: {
