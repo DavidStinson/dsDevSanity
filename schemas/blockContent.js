080e02717f2ef70c6246ff1c2231fcda810b4d41
David Stinson <mail@davidstinson.dev>
2020-05-16 10:59:45 -0500

    fix(schema): remove extra options from blockContent images

--- schemas/blockContent.js
+++ schemas/blockContent.js
@@ -72,9 +72,6 @@ export default {
           options: {
             isHighlighted: true,
           },
-          options: {
-            isHighlighted: true,
-          },
         },
       ],
     },
