fe3a3439697a021bf9dad97d0d3edfa4720d4c46
David Stinson <mail@davidstinson.dev>
2020-05-15 19:31:43 -0500

    build(schema): add description to fields of post

--- schemas/post.js
+++ schemas/post.js
@@ -7,9 +7,11 @@ export default {
       name: "title",
       title: "Title",
       type: "string",
+      description: "The title for this post"
     },
     {
       name: "slug",
+      description: "The uri of this post",
       title: "Slug",
       type: "slug",
       options: {
@@ -19,13 +21,15 @@ export default {
     },
     {
       name: "author",
+      description: "Author or authors of this post",
       title: "Author",
-      type: "reference",
-      to: { type: "author" },
+      type: "array",
+      of: [{ type: "reference", to: {type: "author"} }],
     },
     {
-      name: "mainImage",
-      title: "Main image",
+      name: "heroImage",
+      description: "The main image for this post.",
+      title: "Hero image",
       type: "image",
       options: {
         hotspot: true,
@@ -33,23 +37,27 @@ export default {
     },
     {
       name: "topics",
+      description: "Broad topics of this post",
       title: "Topics",
       type: "array",
       of: [{ type: "reference", to: { type: "topic" } }],
     },
     {
       name: "tags",
+      description: "Specific tags for this post",
       title: "Tags",
       type: "array",
       of: [{ type: "reference", to: { type: "tag" } }],
     },
     {
       name: "publishedDate",
+      description: "The original publish date of this post",
       title: "Published Date",
       type: "date",
     },
     {
       name: "updatedDate",
+      description: "The date of the most recent update to this post, if any",
       title: "Updated Date",
       type: "date",
       validation: (Rule) =>
@@ -57,22 +65,26 @@ export default {
     },
     {
       name: "bodyExcerpt",
+      description: "A piece of highlighted content from the body of this post",
       title: "Body Excerpt",
       type: "blockContent",
     },
     {
       name: "body",
+      description: "The main content of this post",
       title: "Body",
       type: "blockContent",
     },
     {
       name: "githubLink",
+      description: "A GitHub link for any project in this post",
       title: "GitHub Link",
       type: "url",
       scheme: ["https"],
     },
     {
       name: "deploymentLink",
+      description: "A demo of the content of this post",
       title: "Deployment Link",
       type: "url",
       scheme: ["https"],
